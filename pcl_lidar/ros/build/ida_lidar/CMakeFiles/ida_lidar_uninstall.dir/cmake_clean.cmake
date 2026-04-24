f6ebac3fe2a1293ee714440bd383aa177ee31acd
Yalın Öner <yalinoner@gmail.com>
2026-04-24 11:32:30 +0000

    gitignore applied and node can now be built as is

--- pcl_lidar/ros/build/ida_lidar/CMakeFiles/ida_lidar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ida_lidar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ida_lidar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
